54cc3728a145893113e7cf6adb727e7594d141cf
Turbstructor <ycw0201@naver.com>
2024-06-12 16:21:40 +0900

    :tada: Initial commit

---
+++ src/main/kotlin/spartacodingclub/nbcamp/kotlinspring/project/fco234/gameboard/GameBoardApplication.kt
@@ -0,0 +1,11 @@
+package spartacodingclub.nbcamp.kotlinspring.project.fco234.gameboard
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GameBoardApplication
+
+fun main(args: Array<String>) {
+    runApplication<GameBoardApplication>(*args)
+}
