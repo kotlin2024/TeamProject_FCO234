739849d599b15f6ca267a4eff04222a7f5e9f4c4
hysup <Hysup1021@naver.com>
2024-06-14 21:47:31 +0900

    :white_check_mark: 관리자 전용 기능 추가 및 미완성 존재

---
+++ src/main/kotlin/spartacodingclub/nbcamp/kotlinspring/project/fco234/gameboard/domain/admin/model/ChannelRole.kt
@@ -0,0 +1,13 @@
+package spartacodingclub.nbcamp.kotlinspring.project.fco234.gameboard.domain.admin.model
+
+import jakarta.persistence.Entity
+import jakarta.persistence.EnumType
+import jakarta.persistence.Enumerated
+import jakarta.persistence.Table
+
+
+@Entity
+@Table(name = "channel_role")
+class ChannelRole {
+
+}
